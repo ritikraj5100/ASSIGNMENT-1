856f839c2060acdcc3ae03392954cd89ff4caa7e
ritikraj5100 <98815943+ritikraj5100@users.noreply.github.com>
2023-03-30 22:17:13 +0530

    DAY-4 ASSIGNMENT COMPLETED

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int x = 51; 
+        
+        int y = 70;
+        
+        int sum = x + y;
+        
+        System.out.println("Sum is " + sum);
+    }
+}
